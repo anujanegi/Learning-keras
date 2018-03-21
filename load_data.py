5efa8ef045c63c58a01781de013fe2591663a637
anujanegi <anujanegi1@gmail.com>
2018-03-21 17:33:38 +0530

    Changed to functional

--- load_data.py
+++ load_data.py
@@ -1,3 +1,4 @@
+import numpy
 def fix_seed():
 	#fixing a random seed for reproductibility
 	numpy.random.seed(7)
@@ -8,3 +9,4 @@ def load_data():
 	dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 	X = dataset[:,0:8]
 	Y = dataset[:,8]
+	return X, Y
