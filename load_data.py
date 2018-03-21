e7a14baadbe05f77004db9ee827c26f08735086f
anujanegi <anujanegi1@gmail.com>
2018-03-21 16:46:21 +0530

    Load data

---
+++ load_data.py
@@ -0,0 +1,10 @@
+def fix_seed():
+	#fixing a random seed for reproductibility
+	numpy.random.seed(7)
+
+def load_data():
+	#using the Pima Indian dataset from the UCI Machine Learning repository
+	#loading dataset
+	dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
+	X = dataset[:,0:8]
+	Y = dataset[:,8]
